1847cdbe70c4c95a3dca8fc1a3c0e19493ca857d
danghanh23 <ledanghanh@g.densan-ginza.co.jp>
2023-07-31 22:31:45 +0900

    budget page

--- lib/pages/budget_page/budget_page.dart
+++ lib/pages/budget_page/budget_page.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 
-class BudgetPage extends StatelessWidget {
+class BudgetPage extends StatefulWidget {
   const BudgetPage({
     super.key,
     this.child,
@@ -9,9 +13,200 @@ class BudgetPage extends StatelessWidget {
   final Widget? child;
 
   @override
+  State<BudgetPage> createState() => _BudgetPageState();
+}
+
+class _BudgetPageState extends State<BudgetPage> {
+  TextEditingController textEditingController = new TextEditingController();
+  TextEditingController groupController = new TextEditingController();
+  DateTime currentDate = DateTime.now();
+  @override
+  void initState() {
+    super.initState();
+    textEditingController.text = '0';
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Text('budge'),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(),
+        color: const Color.fromARGB(255, 245, 242, 242),
+      ),
+      height: 800.h,
+      child: Column(
+        children: [
+          Container(
+            height: 60.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 10.h,
+                bottom: 10.h,
+                right: 15.h,
+                left: 15.h,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 55.w,
+                    height: 40.h,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 223, 218, 218),
+                      borderRadius: BorderRadius.circular(10.sp),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.wallet,
+                          color: Colors.orange,
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 5.h),
+                          child: Transform.rotate(
+                            angle: -90 * pi / 180,
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              size: 13.sp,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "Budget",
+                    style: TextStyle(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    width: 55.w,
+                    height: 40.h,
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Add",
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 10.w,
+              right: 10.w,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "A financial plan helps you balance your income and expenses.",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  "Budget only supports basic wallet and affiliate wallet.",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30.h,
+          ),
+          Container(
+            height: 60.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 10.h,
+                bottom: 10.h,
+                right: 15.h,
+                left: 15.h,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.add_circle,
+                    color: Colors.green,
+                  ),
+                  SizedBox(
+                    width: 5.w,
+                  ),
+                  Text(
+                    "Add Budget",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.h,
+          ),
+          Container(
+            height: 60.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 10.h,
+                bottom: 10.h,
+                right: 15.h,
+                left: 15.h,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Finished",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/setting_page/setting_page.dart
+++ lib/pages/setting_page/setting_page.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 
-class SettingPage extends StatelessWidget {
+class SettingPage extends StatefulWidget {
   const SettingPage({
     super.key,
     this.child,
@@ -9,9 +13,79 @@ class SettingPage extends StatelessWidget {
   final Widget? child;
 
   @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  TextEditingController textEditingController = new TextEditingController();
+  TextEditingController groupController = new TextEditingController();
+  DateTime currentDate = DateTime.now();
+  @override
+  void initState() {
+    super.initState();
+    textEditingController.text = '0';
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Text('setting'),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(),
+        color: const Color.fromARGB(255, 245, 242, 242),
+      ),
+      height: 800.h,
+      child: Column(
+        children: [
+          Container(
+            height: 60.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 10.h,
+                bottom: 10.h,
+                right: 15.h,
+                left: 15.h,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Setting",
+                    style: TextStyle(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 10.w,
+              right: 10.w,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "Comming soon",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
